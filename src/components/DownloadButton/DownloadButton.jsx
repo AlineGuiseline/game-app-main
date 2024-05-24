855ea47a14760908fc1fe483a46e8fd4139e4fdc
Aline Guiseline <alineguiseline@gmail.com>
2024-05-24 17:12:07 -0300

    switch axios to fetch

--- src/components/DownloadButton/DownloadButton.jsx
+++ src/components/DownloadButton/DownloadButton.jsx
@@ -1,27 +1,27 @@
 import { useEffect, useState } from "react";
-import axios from 'axios';
+//import axios from 'axios';
 
 import { Button} from "./styles";
 
 import LoadingSpinner from "../LoadingSpinner/LoadingSpinner";
 
 const DownloadButton = ({ correctAnswersCount, totalQuestions, onDownloadComplete }) => {
-    const [info, setInfo] = useState([]);
+  const [info, setInfo] = useState([]);
 
-    const isButtonEnabled = correctAnswersCount >= 3;
+  const isButtonEnabled = correctAnswersCount >= 3;
 
-    useEffect(() => {
-        const storedInfo = localStorage.getItem("storedInfo");
+  useEffect(() => {
+    const storedInfo = localStorage.getItem("storedInfo");
 
-        if (storedInfo){
-          try {
-            setInfo(JSON.parse(storedInfo))
-          } catch (error) {
-            console.error("Invalid JSON in storedInfo:", error);
-          }   
-        }
-    }, []);
-
+    if (storedInfo){
+      try {
+        setInfo(JSON.parse(storedInfo))
+      } catch (error) {
+        console.error("Invalid JSON in storedInfo:", error);
+      }   
+    }
+  }, []);
+/*
     const handleFimDoJogo = async () => {
       setIsLoading(true); // Inicia o loading
 
@@ -56,18 +56,65 @@ const DownloadButton = ({ correctAnswersCount, totalQuestions, onDownloadComplet
           setIsLoading(false); // Termina o loading
         }
       }
+*/
 
-      const [isLoading, setIsLoading] = useState(false);
-      
-      return (
-        <>
-              <Button
-              disabled={!isButtonEnabled}
-              onClick={handleFimDoJogo}>Baixar certificado</Button>
+  const handleFimDoJogo = async () => {
+    setIsLoading(true); // Inicia o loading
 
-              {isLoading && <LoadingSpinner />}
-        </>
-      )
+    const percentageCorrect = ((correctAnswersCount / totalQuestions) * 100).toFixed(0); // Calcula a porcentagem de acertos
+
+    try {
+        const response = await fetch('https://game-pro-api.vercel.app/gerar_certificado/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                nome: info[0],
+                sobrenome: info[1],
+                registro: info[2],
+                respostas: percentageCorrect
+            }),
+        });
+
+        if (!response.ok) {
+            throw new Error('Erro na solicitação');
+        }
+
+        const pdfBlob = await response.blob();
+        const pdfUrl = URL.createObjectURL(pdfBlob);
+
+        const downloadLink = document.createElement('a');
+        downloadLink.href = pdfUrl;
+        downloadLink.download = `Certificado ${info[0]}.pdf`;
+
+        downloadLink.click();
+
+        URL.revokeObjectURL(pdfUrl);
+
+        if (onDownloadComplete) {
+            setTimeout(() => {
+                onDownloadComplete();
+            }, 2000);
+        }
+    } catch (error) {
+        console.error('Erro ao enviar solicitação:', error);
+    } finally {
+        setIsLoading(false); // Termina o loading
+    }
   }
 
-  export default DownloadButton;
+  const [isLoading, setIsLoading] = useState(false);
+        
+  return (
+    <>
+      <Button
+      disabled={!isButtonEnabled}
+      onClick={handleFimDoJogo}>Baixar certificado</Button>
+
+      {isLoading && <LoadingSpinner />}
+    </>
+  )
+}
+
+export default DownloadButton;
